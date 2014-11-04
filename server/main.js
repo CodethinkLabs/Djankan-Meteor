2a4b19b36f27ce314ac2d4fd29594f6299b1282f
William Holland <william.holland@codethink.co.uk>
2014-11-04 15:56:02 +0000

    Users
    Making use of the user API method get to usernames into a collection
    so that a name can be displayed on cards rather than just an id number

--- server/main.js
+++ server/main.js
@@ -15,17 +15,22 @@ Meteor.startup(function() {
     Boards.remove({});
     Lanes.remove({});
     Cards.remove({});
-    board_url = HOST+API+'boards/'+BOARD_ID;
+    Checklists.remove({});
+    Assignees.remove({});
+    Users.remove({});
+    users_url = HOST+API+'users/';
+    board_url = HOST+API+'boards/'+BOARD_ID+'/';
     lanes_url = board_url+'lanes/';
     cards_url = board_url+'cards/';
 
+    getToCollection(users_url,Users);
     getToCollection(board_url,Boards);
     getToCollection(lanes_url,Lanes);
     getToCollection(cards_url,Cards);
     cards = Cards.find({});
     cards.forEach(function (card) {
         card_id = card.header.cardNumber;
-        console.log("card.id:"+card.id);
+        console.log(card_id);
         checklist_url = HOST+API+'card/'+card_id+'/checklists/';
         assignees_url = HOST+API+'card/'+card_id+'/assignees/';
         getToCollection(checklist_url,Checklists);
