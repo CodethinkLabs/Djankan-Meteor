541ed2030f559d4bf22af0a2c057a3dc29aa96bd
William Holland <william.holland@codethink.co.uk>
2014-12-15 09:18:18 +0000

    renamed chosenBoard method to boardView
    For clarity of what it does

--- client/templates/mainTemplate.js
+++ client/templates/mainTemplate.js
@@ -22,7 +22,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
 Template.mainTemplate.helpers({
-    chosenBoard: function () {
+    boardView: function () {
         if(Session.get("boardId"))
             return true;
         return false;
