25d6e9fa20972b78ffe41865241be0066b77b4b7
William Holland <william.holland@codethink.co.uk>
2015-01-14 11:38:41 +0000

    removed pointless assertion

--- server/admintools.js
+++ server/admintools.js
@@ -1,8 +1,6 @@
 djankan = {
     // creates a new meteor user, triggering hook making new userprofile
     newUser : function(newUserEmailAddress) {
-        existingEmail = UserProfiles.findOne({email:newUserEmailAddress});
-        if(existingEmail) throw "email already in use";
         Accounts.createUser({
             email:newUserEmailAddress,
             password:"password"
